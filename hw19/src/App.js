3607b2d5a82b0d2428ed820a8eb8da61e7aa8dc2
Leannaland <leannaland2011@gmail.com>
2021-01-22 20:38:59 -0500

    completed

--- hw19/src/App.js
+++ hw19/src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
-import Header from "./components/Header";
-import EmployeeList from "./components/EmployeeList";
+ import Header from "./components/Header";
+ import EmployeeList from "./components/EmployeeList";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <EmployeeList />
+        { <Header /> }
+        {<EmployeeList />}
       </div>
     );
   }
